78efc4ab703e3d0ff3a69212e3f2b4e6a2e15475
Hendro Smit <smit.gamedev@gmail.com>
2021-10-11 11:40:19 +0200

    Dice functionality 1.1
    An update command visibility and add whisper option

--- commands/dice.js
+++ commands/dice.js
@@ -3,11 +3,14 @@ const Roll = require('roll'), roll = new Roll();
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('roll')
+		.setName('dice')
 		.setDescription('A dice roller')
-		.addStringOption(option => option.setName('dice').setDescription('The dice you want to roll in dice notation')),
+		.addStringOption(option => option.setName('input').setDescription('The dice you want to roll in dice notation'))
+		.addBooleanOption(option => option.setName('whisper').setDescription('Whisper the roll to yourself?')),
 	async execute(interaction) {
-		const result = roll.roll(interaction.options.getString('input'));
-		return interaction.reply(result);
+		const input = interaction.options.getString('input');
+		const whisper = interaction.options.getBoolean('whisper');
+		const result = roll.roll(input);
+		return interaction.reply({ content: `Rolling \`${input}\` : \`${result}\``, ephemeral: whisper });
 	},
 };

644359694575866445a0f1a65c7ff4933c32e584
Hendro Smit <smit.gamedev@gmail.com>
2021-10-27 10:47:01 +0200

    Require path fix

--- commands/schedule.js
+++ commands/schedule.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { agenda } = require('../Agenda');
+const { agenda } = require('../index');
 
 module.exports = {
 	data: new SlashCommandBuilder()
